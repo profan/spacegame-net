84acd738bc83a990a1dba7567648510d29062281
Robin Hübner <profan@prfn.se>
2018-02-23 19:47:42 +0100

    show connecting to address as well

--- states/connecting.gd
+++ states/connecting.gd
@@ -1,11 +1,14 @@
 extends Control
 
+onready var title = get_node("center/title")
+
 var from_scene_name = false
 
 func _ready():
 	Net.join_server("127.0.0.1", Net.DEFAULT_HOST_PORT)
 	Net.connect("on_connection_successful", self, "_on_connection_successful")
 	Net.connect("on_connection_failure", self, "_on_connection_failure")
+	title.text = "Connecting to: %s..." % Net.get_host_ip()
 
 func _unhandled_input(event):
 	if event is InputEventKey:
